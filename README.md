b405db82d93d11fd89d3c62c248b9fa6ed740bc8
tcgwl <tcgwl_123@126.com>
2018-05-24 18:30:36 +0800

    抽象类与接口

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # MyKotlinDemo  
 ## kotlin语法练习  
 ### Kotlin系统入门与进阶  
-[慕课网实战教程](https://coding.imooc.com/class/108.html)  
 
 - 第1章 课程介绍  
 - 第2章 数据类型  
@@ -10,10 +9,6 @@
 - 第5章 高阶函数  
 - 第6章 领域特定语言 DSL  
 - 第7章 协程 Coroutine  
-- 第8章 反射语法与库  
-- 第9章 泛型，型变，星投影  
-- 第10章 Kotlin与 Java 混合开发  
-- 第11章 Kotlin小案例展示与Kotlin未来展望  
-- 第12章 Kotlin1.2跨平台新特性示例  
-
+- 第8章 Kotlin 与 Java 混合开发  
+- 第9章 Kotlin的应用与展望  
 
---
+++ src/main/kotlin/net/println/kotlin/chapter04/AbstractAndInterfaceExample.kt
@@ -0,0 +1,81 @@
+package net.println.kotlin.chapter04
+
+import java.lang.IllegalArgumentException
+
+/**
+ * 抽象类与接口(半成品与协议)
+ *
+ * 接口，直观理解就是一种约定
+ * 不能有状态
+ * 必须由类对其进行实现后使用
+ *
+ * 抽象类
+ * 实现了部分协议的半成品
+ * 可以有状态，可以有方法实现
+ * 必须由子类继承后使用
+ *
+ * 抽象类与接口的共性
+ * 1.比较抽象，不能直接实例化
+ * 2.有需要子类(实现类)实现的方法
+ * 3.父类(接口)变量可以接受子类(实现类)的实例赋值
+ *
+ * 抽象类与接口的区别
+ * 1.抽象类有状态，接口无状态
+ * 2.抽象类有方法实现，接口只能有无状态的默认实现
+ * 3.抽象类只能单继承，接口可以多实现
+ * 4.抽象类反映本质，接口体现能力(从设计思想角度考虑)
+ *
+ * 直接的说，如果用抽象类与接口来描述一个东西，
+ * 那么这个东西的中心词前面的修饰词就是接口要负责的部分，
+ * 中心词就是抽象类要负责的部分。
+ * 例如：<联想><笔记本>电脑
+ * 用'联想'和'笔记本'分别做一个接口，'电脑'做一个抽象父类
+ */
+
+//输入设备接口
+interface InputDevice {
+    fun input(event: Any)
+}
+//usb接口
+interface USBInputDevice: InputDevice
+//蓝牙
+interface BLEInputDevice: InputDevice
+
+abstract class USBMouse(val name: String): USBInputDevice, OpticalMouse {
+    override fun input(event: Any) {
+
+    }
+
+    override fun toString(): String {
+        return name
+    }
+}
+
+class LogitechMouse: USBMouse("罗技鼠标")
+
+//光电鼠标
+interface OpticalMouse
+
+class Computer {
+    fun addUSBInputDevice(inputDevice: USBInputDevice) {
+        println("add usb input device: $inputDevice")
+    }
+    fun addBLEInputDevice(inputDevice: BLEInputDevice) {
+        println("add ble input device: $inputDevice")
+    }
+
+    fun addInputDevice(inputDevice: InputDevice) {
+        when(inputDevice) {
+            is USBInputDevice -> addUSBInputDevice(inputDevice)
+            is BLEInputDevice -> addBLEInputDevice(inputDevice)
+            else -> throw IllegalArgumentException("输入设备类型不支持")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val computer = Computer()
+//    val mouse = USBMouse("罗技鼠标")
+    val mouse = LogitechMouse()
+    computer.addInputDevice(mouse)
+}
